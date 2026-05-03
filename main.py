27f48b53d33d800b52493879f56c3c4dd230557f
farmer_2010 <155635633+farmer2010@users.noreply.github.com>
2026-05-03 22:13:12 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import copy
 import pyperclip
 
-words = ["if", "else", "while", "num", "str", "bool", "obj", "id", "break", "continue", "null", "print", "printflush", "stop", "end"]
+words = ["if", "else", "while", "for", "num", "str", "bool", "obj", "id", "break", "continue", "null", "print", "printflush", "stop", "end"]
 symb = ["{", "}", "(", ")", "=", "+=", "-=", "*=", "/=", "%=", "~/=", "+", "-", "*", "**", "/", "~/", "%", "%%", "==", ">", "<", ">=", "<=", "!=", "'", '"', "++", "--", "===", "<<", ">>", ">>>", "!", "&&", "||", "&", "^", ";"]
 operations = ["+", "-", "*", "/", "~/", "%", "%%", "**", ">", "<", ">=", "<=", "==", "===", "!=", "!", "&&", "||", "^", "&", "<<", ">>", ">>>"]
 operations_1_param = ["!"]
@@ -52,7 +52,9 @@ prios = {
 }
 
 class Token():
-    def __init__(self, text):
+    def __init__(self, text, line=0, pos=0):
+        self.line = line
+        self.pos = pos
         self.type = None
         number = 1
         try:
@@ -85,6 +87,20 @@ class Token():
     def __repr__(self):
         return("'" + self.text + "'")
 
+def get_error(err_code, line_ind, pos, line):
+    res = f"Syntax error (code {err_code}) on line {line_ind}:\n"
+    res += line + "\n"
+    res += " " * pos + "^\n"
+    if err_code == 10:
+        pass
+    elif err_code == 11:
+        pass
+    elif err_code == 20:
+        res += 'Unknown command ""'
+    elif err_code == 30:
+        res += "Invalid number format"
+    return(res)
+
 #+   1 уровень - разделение кода на блоки, удаление комментариев и пробелов перед строками
 #+   2 уровень - токенайзер
 #+   3 уровень - разделение цикла for
@@ -102,7 +118,8 @@ class Token():
 1x - неверное количество скобок
 2x - неизвестная команда
 3x - неправильный формат числа
-4x - 
+4x - отсутствует ; в конце строки
+5x - неверное количество кавычек
 
 10 - неверное количество фигурных скобок
 11 - неверное количество круглых скобок
@@ -111,6 +128,11 @@ class Token():
 20 - неизвестная команда
 
 30 - неправильный формат числа
+
+40 - отсутствует ;
+
+50 - неверное количество '
+51 - неверное количество "
 '''
 
 def compile(code):
@@ -120,41 +142,46 @@ def compile(code):
     #
     lines_level1 = []
     line = ""
-    string = None
+    string = 0
     start = 1
     comment = 0
     last_symbol = ""
     for_cycle = 0
+    line_ind = 0
+    lines_indexes = []
     for symbol in code:
+        if symbol == "\n":
+            line_ind += 1
         if not comment:
             if symbol != " " and symbol != "\t" and symbol != "\n":
                 start = 0
             if symbol == "'" or symbol == '"':
-                if string == None:
-                    string = symbol
-                elif string == symbol:
-                    string = None
-            if string == None and not for_cycle:
+                string = not string
+            if string == 0 and not for_cycle:
                 if symbol == ";":
+                    lines_indexes.append(line_ind)
                     lines_level1.append(line)
                     line = ""
                     start = 1
                 elif symbol == "{":
+                    lines_indexes.append(line_ind)
                     lines_level1.append(line + "{")
                     line = ""
                     start = 1
             if for_cycle and symbol == "{":
                 for_cycle = 0
+                lines_indexes.append(line_ind)
                 lines_level1.append(line + "{")
                 line = ""
                 start = 1
-            if symbol == "}" and string == None:
+            if symbol == "}" and string == 0:
+                lines_indexes.append(line_ind)
                 lines_level1.append(line + "}")
                 line = ""
                 start = 1
-            if (((symbol != ";" or for_cycle) and symbol != "}") or string != None) and not start:
+            if (((symbol != ";" or for_cycle) and symbol != "}") or string) and not start:
                 line += symbol
-            if symbol == "/" and last_symbol == "/" and string == None:
+            if symbol == "/" and last_symbol == "/" and string == 0:
                 comment = 1
             if line == "for":
                 for_cycle = 1
@@ -168,10 +195,11 @@ def compile(code):
     #токенайзер
     #
     lines_level2 = []
-    for l in lines_level1:
+    for li in range(len(lines_level1)):
+        l = lines_level1[li]
         line = ""
         cmd = []
-        txt = None
+        txt = 0
         token_type = None
         for i in range(len(l)):
             symbol = l[i]
@@ -181,10 +209,7 @@ def compile(code):
                 next_symbol = ""
             #
             if symbol == "'" or symbol == '"':
-                if txt == None:
-                    txt = symbol
-                elif txt == symbol:
-                    txt = None
+                txt = not txt
             #
             if token_type == None:
                 if symbol == "'" or symbol == '"':
@@ -208,7 +233,7 @@ def compile(code):
             #
             if (token_type == "word" and (next_symbol in spaces or next_symbol in symb)) or \
                     (token_type == "special symbol" and not line + next_symbol in symb) or \
-                    (token_type == "string" and (symbol == "'" or symbol == '"') and txt == None) or \
+                    (token_type == "string" and (symbol == "'" or symbol == '"') and txt == 0) or \
                     (token_type == "number" and (next_symbol in spaces or next_symbol in symb)) or \
                     i == len(l) - 1:
                 if line != "":
@@ -216,9 +241,16 @@ def compile(code):
                         try:
                             float(line)
                         except:
-                            console += "Error 30"
+                            console += get_error(30, lines_indexes[li], 0, code.split("\n")[lines_indexes[li]])
                             return([], console)
-                    cmd.append(Token(line))
+                    elif token_type == "string":
+                        if line[0] != line[-1]:
+                            if line[0] == "'":
+                                console += get_error(50, lines_indexes[li], 0, code.split("\n")[lines_indexes[li]])
+                            elif line[0] == '"':
+                                console += get_error(51, lines_indexes[li], 0, code.split("\n")[lines_indexes[li]])
+                            return([], console)
+                    cmd.append(Token(line, lines_indexes[li]))
                     token_type = None
                     line = ""
         lines_level2.append(cmd)
@@ -227,7 +259,8 @@ def compile(code):
     #проверка синтаксиса
     #
     fig_bra_count = 0
-    for l in lines_level2:
+    for li in range(len(lines_level2)):
+        l = lines_level2[li]
         circ_bra_count = 0
         for j in range(len(l)):
             token = l[j]
@@ -247,13 +280,13 @@ def compile(code):
                 circ_bra_count -= 1
             #
             if token.type == "variable" and next_token != "=" and j == 0:
-                console += "Errorr 20"
+                console += get_error(20, lines_indexes[li], 0, code.split("\n")[lines_indexes[li]])
                 return([], console)
         if circ_bra_count != 0:
-            console += "Error 11"
+            console += get_error(11, lines_indexes[li], 0, code.split("\n")[lines_indexes[li]])
             return([], console)
     if fig_bra_count != 0:
-        console += "Error 10"
+        console += get_error(10, -1, 0, code.split("\n")[lines_indexes[li]])
         return([], console)
     #
     #третий уровень
@@ -615,7 +648,12 @@ def compile(code):
 
 
 text = """
-num a = 1.0;
+for (num i = 0; i < 10; i++){
+print(i + 2*2);
+}
+stop();
+num a = 0;
+a = 22333 + 1;
 """
 
 res = compile(text)
